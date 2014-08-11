e854653b34ca54598aa39a5dbc650f244d7ada1c
Jeremy Fairbank <elpapapollo@gmail.com>
2014-08-10 22:29:14 -0400

    Build lib for initial release

--- dist/marionette.component.js
+++ dist/marionette.component.js
@@ -5,37 +5,139 @@
  * @link https://github.com/jfairbank/marionette.component
  * @license ISC
  */
-  // Marionette.Component
-// --------------------
-// An object to represent an application component, typically 
-// something visual, encapsulated in to an object that can be 
-// instantiated and dispalyed on screen as needed.
-//
-// Marionette.Component can optionally have a `region`, `model`,
-// and/or `collection` passed to it through the constructor options.
 
-var Component = Marionette.Controller.extend({
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(["underscore","backbone.marionette"], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('underscore'), require('backbone.marionette'));
+  } else {
+    root.Marionette.Component = factory(root._, root.Marionette);
+  }
+}(this, function(_, Marionette) {
+
+// Marionette.Component
+// --------------------
+// A reusable component object that encapsulates a view and maintains
+// business logic on itself instead of the view. A `Marionette.Component`
+// can have an optional model and collection too.
+
+Marionette.Component = Marionette.Object.extend({
   constructor: function(options) {
     options = options || {};
 
-    this.region     = options.region;
     this.model      = options.model;
     this.collection = options.collection;
 
-    Marionette.Controller.prototype.constructor.apply(this, arguments);
+    Marionette.Object.prototype.constructor.apply(this, arguments);
   },
 
-  show: function() {
-    this.showLayout();
+  // Show this component inside a region
+  showIn: function(region) {
+    if (this._isShown) {
+      throw new Error('This component is already shown in a region.');
+    }
+
+    if (!region) {
+      throw new Error('Please supply a region to show inside.');
+    }
+
+    this.region = region;
+
+    this.triggerMethod('before:show');
+
+    this._showView();
+    this._isShown = true;
+
+    this.triggerMethod('show');
   },
 
-  showLayout: function() {
-    var layout = this.layout = this.getLayout();
+  // Destroy the component and view
+  destroy: function() {
+    if (this._isDestroyed) {
+      return;
+    }
 
-    this.listenTo(layout, 'show', function() {
-      this.triggerMethod('show:layout');
+    this.triggerMethod('before:destroy');
+
+    this._destroyViewThroughRegion();
+    this._removeReferences();
+
+    this.triggerMethod('destroy');
+    this.stopListening();
+
+    this._isDestroyed = true;
+  },
+
+  // Show the view in the region
+  _showView: function() {
+    var view = this.view = this._getView();
+
+    this._initializeViewEvents();
+
+    // Trigger show:view after the view is shown in the region
+    this.listenTo(view, 'show', _.partial(this.triggerMethod, 'show:view'));
+
+    // Trigger before:show before the region shows the view
+    this.triggerMethod('before:show:view');
+
+    // Show the view in the region
+    this.region.show(view);
+
+    // Destroy the component if the region is emptied because it destroys
+    // the view
+    this.listenToOnce(this.region, 'empty', this.destroy);
+  },
+
+  // Get an instance of the view to display
+  _getView: function() {
+    var ViewClass = this.viewClass;
+
+    if (!ViewClass) {
+      throw new Error('You must specify a viewClass for your component.');
+    }
+
+    return new ViewClass({
+      model: this.model,
+      collection: this.collection
     });
+  },
 
-    this.region.show(layout);
+  // Set up events from the `viewEvents` hash
+  _initializeViewEvents: function() {
+    if (this.viewEvents) {
+      this.bindEntityEvents(this.view, this.viewEvents);
+    }
+  },
+
+  // Destroy a view by emptying the region
+  _destroyViewThroughRegion: function() {
+    var region = this.region;
+
+    // Don't do anything if there isn't a region or view.
+    // We need to check the view or we could empty the region before we've
+    // shown the component view. This would destroy an existing view in the
+    // region.
+    if (!region || !this.view) {
+      return;
+    }
+
+    // Remove listeners on region, so we don't call `destroy` a second time
+    this.stopListening(region);
+
+    // Destroy the view by emptying the region
+    region.empty();
+  },
+
+  // Remove references to all attached objects
+  _removeReferences: function() {
+    delete this.model;
+    delete this.collection;
+    delete this.region;
+    delete this.view;
   }
 });
+
+return Marionette.Component;
+
+}));
